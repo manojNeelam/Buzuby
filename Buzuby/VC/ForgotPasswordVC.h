4e4bc48ff576f4990a9a14d48685736f07e3768a
Manoj <manojneelam143@gmail.com>
2016-05-26 18:31:57 +0530

    Forgot password: added username

--- Buzuby/VC/ForgotPasswordVC.h
+++ Buzuby/VC/ForgotPasswordVC.h
@@ -14,5 +14,6 @@
 
 @property (weak, nonatomic) IBOutlet UIButton *btnForgotPassword;
 - (IBAction)onClickForgotPasswordButton:(id)sender;
+@property (weak, nonatomic) IBOutlet DefaultTextField *txtFldUserName;
 
 @end
